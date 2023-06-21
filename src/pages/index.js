e3c78c915bd14a5c0796966102b5090798865988
YokYok <patiparn.wong@gmail.com>
2023-06-21 11:41:25 +0700

    Initial commit from Create Next App

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import "../app/globals.css";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <>
+
+      <Header />
+      <Footer />
+    </>
+  );
+}
